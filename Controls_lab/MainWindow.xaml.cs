8a650a2e5e073debd52ee0f7f7dfbc16844e6ea0
Василий <fcss.void@gmail.com>
2021-11-17 17:36:30 +0300

    Добавьте файлы проекта.

---
+++ Controls_lab/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls_lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+                textBox.FontSize = fontSize;
+        }
+
+        private void Button_Bold(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void Button_Italic(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+        private void Button_Underline(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Underline)
+                textBox.TextDecorations = null;
+            else
+                textBox.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void btn_black_Checked(object sender, RoutedEventArgs e)
+        {
+            if (btn_blue != null)
+            {
+                if (!btn_blue.IsPressed && textBox != null)
+                    textBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void btn_blue_Checked(object sender, RoutedEventArgs e)
+        {
+            if (btn_black != null)
+            {
+                if (!btn_black.IsPressed && textBox != null)
+                    textBox.Foreground = new SolidColorBrush(Colors.Blue);
+            }
+        }
+    }
+}
